5d7bbbea944d27e38527e9d3bd1a85dacdc54844
Mamontov Andrey <andrvm@andrvm.ru>
2022-03-20 17:55:03 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Typescript fibonacci sequence number with memorization example
+ */
+
+const fibonacci = (n, memo = {}) => {
+  if (n === 1) {
+    return [0, 1];
+  } else {
+    memo[n] = fibonacci(n - 1, memo);
+    memo[n].push(memo[n][memo[n].length - 1] + memo[n][memo[n].length - 2]);
+    return memo[n];
+  }
+}
+
+console.log(fibonacci(50));
+
+
